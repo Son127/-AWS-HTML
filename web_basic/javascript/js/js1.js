b6c5baa075f2a313ff0a20a6d2c0649967aa29bf
A01051067504@gamil.com <A01051067504@gamil.com>
2023-03-13 12:51:44 +0900

    03-13 js

---
+++ web_basic/javascript/js/js1.js
@@ -0,0 +1,22 @@
+// 무조건 순서대로 실행이 된다 
+// alert("알림창을 확인해주세요.");
+// alert("두번째 알림창.");
+// alert(10 + 23);
+//  객체를 확인하거나 배열안에 값을 확인할때 사용
+// console.log("println");
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+
+
+let test;
+console.log(test); //undefined : 자료형이 선언이 안되었을 경우
+
+//  정수 형변환(parseInt or Number)
+let num1 = parseInt (prompt("첫번재 숫자"));
+let num2 = parseInt (prompt("첫번재 숫자"));
+let num3 = Number (prompt("첫번재 숫자"));
+let num4 = Number (prompt("두번재 숫자"));
+console.log( num1 + num2);
+console.log( num3 + num4);
